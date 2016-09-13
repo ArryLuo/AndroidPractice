920a7a32996f4df9cb4f9e4eef371ff4bc3328ba
lyl <iamlyl@foxmail.com>
2016-09-13 14:22:41 +0800

    简化

--- ThemeChange/app/src/main/java/com/lyl/theme/MainActivity.java
+++ ThemeChange/app/src/main/java/com/lyl/theme/MainActivity.java
@@ -7,28 +7,39 @@ import android.view.View.OnClickListener;
 
 public class MainActivity extends AppCompatActivity {
 
+    /**
+     * recreate() 会使 Activity 重新刷新，所以 这个标志必须是静态的。
+     */
+    private static boolean night = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        ThemeUtile.changeTheme(this);
+        changeTheme();
         setContentView(R.layout.activity_main);
 
         findViewById(R.id.email_sign_in_button).setOnClickListener(new OnClickListener() {
             @Override
             public void onClick(View view) {
-                changeTheme();
+                change();
             }
         });
-
     }
 
-
-    private void changeTheme() {
-        ThemeUtile.night = !ThemeUtile.night;
-        ThemeUtile.changeTheme(this);
+    private void change() {
+        night = !night;
+        changeTheme();
         recreate();
     }
 
+    /**
+     * 根据标志改变主题
+     */
+    private void changeTheme() {
+        if (night) {
+            setTheme(R.style.AppThemeNight);
+        } else {
+            setTheme(R.style.AppTheme);
+        }
+    }
 }
-
