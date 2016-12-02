4e3cc332d1c2ed0c813fff5066e126d84d28a730
lyl <iamlyl@foxmail.com>
2016-12-02 09:53:19 +0800

    表格制作

---
+++ MakeTable/app/src/main/java/com/lyl/maketable/MainActivity.java
@@ -0,0 +1,144 @@
+package com.lyl.maketable;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends Activity {
+
+    private NoscrollListView mLeft;
+    private LeftAdapter mLeftAdapter;
+
+    private NoscrollListView mData;
+    private DataAdapter mDataAdapter;
+
+    private SyncHorizontalScrollView mHeaderHorizontal;
+    private SyncHorizontalScrollView mDataHorizontal;
+
+    private List<String> mListData;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        initView();
+    }
+
+    private void initView(){
+        mLeft = (NoscrollListView) findViewById(R.id.lv_left);
+        mData = (NoscrollListView) findViewById(R.id.lv_data);
+        mDataHorizontal = (SyncHorizontalScrollView) findViewById(R.id.data_horizontal);
+        mHeaderHorizontal = (SyncHorizontalScrollView) findViewById(R.id.header_horizontal);
+
+        mDataHorizontal.setScrollView(mHeaderHorizontal);
+        mHeaderHorizontal.setScrollView(mDataHorizontal);
+
+        mListData = new ArrayList<>();
+        mListData.add("1");
+        mListData.add("2");
+        mListData.add("3");
+        mListData.add("4");
+        mListData.add("5");
+        mListData.add("6");
+        mListData.add("7");
+        mListData.add("8");
+        mListData.add("9");
+        mListData.add("10");
+        mListData.add("11");
+        mListData.add("12");
+        mListData.add("13");
+
+        mLeftAdapter= new LeftAdapter();
+        mLeft.setAdapter(mLeftAdapter);
+
+        mDataAdapter = new DataAdapter();
+        mData.setAdapter(mDataAdapter);
+    }
+
+    class LeftAdapter extends BaseAdapter {
+
+        @Override
+        public int getCount() {
+            return mListData.size();
+        }
+
+        @Override
+        public Object getItem(int position) {
+            return mListData.get(position);
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return position;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            ViewHolder holder = null;
+            if (convertView == null) {
+                holder = new ViewHolder();
+                convertView = LayoutInflater.from(MainActivity.this).inflate(R.layout.tv_left, null);
+                holder.tvLeft = (TextView) convertView.findViewById(R.id.tv_left);
+                convertView.setTag(holder);
+            } else {
+                holder = (ViewHolder) convertView.getTag();
+            }
+
+            holder.tvLeft.setText("第" + position + "行");
+
+            return convertView;
+        }
+
+        class ViewHolder {
+            TextView tvLeft;
+        }
+    }
+
+    class DataAdapter extends BaseAdapter {
+
+        @Override
+        public int getCount() {
+            return mListData.size();
+        }
+
+        @Override
+        public Object getItem(int position) {
+            return mListData.get(position);
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return position;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            ViewHolder holder = null;
+            if(convertView == null){
+                holder = new ViewHolder();
+                convertView = LayoutInflater.from(MainActivity.this).inflate(R.layout.item_data, null);
+                holder.tvData = (TextView) convertView.findViewById(R.id.tv_data);
+                holder.linContent = (LinearLayout) convertView.findViewById(R.id.lin_content);
+                convertView.setTag(holder);
+            }else{
+                holder = (ViewHolder) convertView.getTag();
+            }
+
+            return convertView;
+        }
+
+        class ViewHolder {
+            TextView tvData;
+            LinearLayout linContent;
+        }
+    }
+}
